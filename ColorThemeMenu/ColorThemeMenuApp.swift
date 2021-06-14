1f466efff5779793d0337f96fa03c7db7f75499a
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-14 18:33:30 -0300

    Initial Commit

---
+++ ColorThemeMenu/ColorThemeMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorThemeMenuApp.swift
+//  ColorThemeMenu
+//
+//  Created by Raphael Cerqueira on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorThemeMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
